1b23761037addb2d2049d5721cd0eb224b2ca046
Mehmet Alıcı <alc_mehmet@outlook.com>
2022-05-21 12:23:17 +0300

    table entity auto increment fix

--- src/main/java/com/example/demo/entities/Table.java
+++ src/main/java/com/example/demo/entities/Table.java
@@ -4,6 +4,8 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Getter
@@ -11,6 +13,7 @@ import javax.persistence.Id;
 @Entity(name = "tbl")
 public class Table {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
     private int tableId;
