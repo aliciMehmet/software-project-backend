721304a91a0f116dab9a275cbc11ecd5951deaaa
Mehmet Alıcı <alc_mehmet@outlook.com>
2022-05-26 23:09:34 +0300

    tba

--- src/main/java/com/example/demo/api/vo/TableStatusResponseVo.java
+++ src/main/java/com/example/demo/api/vo/TableStatusResponseVo.java
@@ -1,2 +1,13 @@
-package com.example.demo.api.vo;public class TableStatusResponseVo {
+package com.example.demo.api.vo;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+import java.util.Map;
+
+@Getter
+@Setter
+public class TableStatusResponseVo {
+    private List<Map<Integer,Boolean>> tableStatus;
 }
